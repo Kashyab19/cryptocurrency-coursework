ebee0fcd81d16d5e8cba0de789c75a47628834c2
Kashyab19 <kashyabnarayanan3721@gmail.com>
2023-02-27 12:58:10 -0500

    Created v4

---
+++ v4.0/contracts/MintingERC20Tokens.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract MyToken is ERC20 {
+    
+    uint256 immutable maxSupply;
+    uint256 circulatingSupply;
+
+    constructor(uint256 _maxSupply) ERC20("Bid", "BIDS") {
+        maxSupply = _maxSupply;
+    }
+
+    function getMaxSupply() public view returns(uint256){
+        return maxSupply;
+    }
+    function mint(address to, uint256 amount) public {
+        circulatingSupply = totalSupply();
+        require(circulatingSupply < maxSupply, "Max Supply has reached");
+        _mint(to, amount);
+    }
+}
---
+++ v4.0/contracts/MintingERC721Tokens.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract Minting_ERC721Tokens is ERC721, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    uint256 maxSupply;
+
+    constructor(uint256 _maxSupply) ERC721("DutchAuctionTokens", "DAT") {
+        maxSupply = _maxSupply;
+    }
+
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        require(tokenId <= (maxSupply-1), "Max supply quota reached");
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+}
